839841f8b806116051c7635c00e7249cd3b7b9ad
Tomas Okal <tomas.okal@gmail.com>
2020-05-05 14:29:13 -0500

    R CMD CHECK done. Only current issue is installation of data.table from R 3.6.2.

--- R/draft_range.R
+++ R/draft_range.R
@@ -3,11 +3,12 @@
 #' This is general function that scrapes the pro-football-reference website for the year that you specify.
 #' Will return all of the draft picks of that year with round, pick number, team, name, position, age, and college.
 #'
-#' @param year 
+#' @param min First year in range.
+#' @param max Last year in range.
 #'
 #' @importFrom data.table rbindlist
 #'
-#' @return
+#' @return All draft picks from first year to last year in provided range.
 #' @export
 #'
 #' @examples draft_range(min = "2010", max = "2020")
@@ -16,7 +17,7 @@ draft_range <- function(min, max) {
   year_range <- list(seq(min:max))
   year_range <- lapply(year_range, function(x) {as.character(as.numeric(as.character(x)) - 1 + as.numeric(as.character(min)))})
   year_range <- as.list(unlist(year_range))
-  dt_pages <- lapply(year_range, generate_tables)
+  dt_pages <- lapply(year_range, draft_year)
   dt_pages <- data.table::rbindlist(dt_pages)
   
   return(dt_pages)
--- R/draft_year.R
+++ R/draft_year.R
@@ -3,17 +3,19 @@
 #' This is general function that scrapes the pro-football-reference website for the year that you specify.
 #' Will return all of the draft picks of that year with round, pick number, team, name, position, age, and college.
 #'
-#' @param year 
+#' @param year A year.
 #'
 #' @importFrom rvest html_table
 #' @importFrom xml2 read_html
 #' @import data.table
 #'
-#' @return
+#' @return All draft picks provided year.
 #' @export
 #'
 #' @examples draft_year(year = "2020")
 draft_year <- function(year) {
+  
+  V1 <- V2 <- V3 <- V4 <- V5 <- V6 <- V13 <- NULL
   
   url <- paste0("https://www.pro-football-reference.com/years/", year, "/draft.htm")
   page <- rvest::html_table(xml2::read_html(url)
@@ -21,15 +23,15 @@ draft_year <- function(year) {
                             , fill = TRUE
                             )
   dt_page <- data.table::data.table(page[[1]])[!V1 == "Rnd"
-                                               , .(YEAR = as.numeric(year)
-                                                   , ROUND = as.numeric(V1)
-                                                   , OVERALL_PICK = as.numeric(V2)
-                                                   , TEAM = V3
-                                                   , PLAYER = V4
-                                                   , POSITION = V5
-                                                   , AGE = as.numeric(V6)
-                                                   , COLLEGE = V13
-                                                   )
+                                               , list(YEAR = as.numeric(year)
+                                                      , ROUND = as.numeric(V1)
+                                                      , OVERALL_PICK = as.numeric(V2)
+                                                      , TEAM = V3
+                                                      , PLAYER = V4
+                                                      , POSITION = V5
+                                                      , AGE = as.numeric(V6)
+                                                      , COLLEGE = V13
+                                                      )
                                                ]
   
   
